f7f60287bd85fdddc3573709881bbdfe4e3522fa
robert532 <33236215+robert532@users.noreply.github.com>
2019-06-16 14:38:47 -0500

    fix: correctly address those moved base views

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='base/index.html', extra_context={'landing': 'landing', 'headerclass': 'alt'}), name='index'),
-    path('generic.html', TemplateView.as_view(template_name='generic.html'), name='generic'),
-    path('elements.html', TemplateView.as_view(template_name='elements.html'), name='elements'),
-    path('contact.html', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    path('generic.html', TemplateView.as_view(template_name='base/generic.html'), name='generic'),
+    path('elements.html', TemplateView.as_view(template_name='base/elements.html'), name='elements'),
+    path('contact.html', TemplateView.as_view(template_name='base/contact.html'), name='contact'),
 ]
