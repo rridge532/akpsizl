1182d7e76f207f7e259849a7d1e9aec6133b8ddf
Robert <robertridge3@gmail.com>
2019-08-23 02:53:29 -0500

    reorganize rush urls

--- rush/urls.py
+++ rush/urls.py
@@ -1,20 +1,23 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
+
 from . import views
 
 app_name = 'rush'
 urlpatterns = [
-    path('thanks/', views.thanks, name='thanks'),
+    path('', TemplateView.as_view(template_name='rush/portal.html'), name='portal'),
     path('changenight/', views.changenight, name='changenight'),
-    path('interview/', views.interview, name='interview'),
     path('signin/', views.rusheesignin, name='rusheesignin'),
     path('signup/', views.rusheesignup, name='rusheesignup'),
-    path('application/', views.application, name='application'),
-    path('mention/', views.mention, name='mention'),
-    path('vote/', views.vote, name='vote'),
-    path('vote/<int:page>/', views.vote, name='vote'),
-    path('prevote/', views.prevote, name='prevote'),
-    path('prevote/<int:page>/', views.prevote, name='prevote'),
-    path('bids/', views.biddisplay, name='biddisplay'),
     path('powerpoint/', views.powerpoint, name='powerpoint'),
     path('powerpoint/<int:page>/', views.powerpoint, name='powerpoint'),
+    path('bids/', views.biddisplay, name='biddisplay'),
+    path('application/', views.application, name='application'),
+    path('mention/', views.mention, name='mention'),
+    path('interview/', views.interview, name='interview'),
+    path('prevote/', views.prevote, name='prevote'),
+    path('prevote/<int:page>/', views.prevote, name='prevote'),
+    path('vote/', views.vote, name='vote'),
+    path('vote/<int:page>/', views.vote, name='vote'),
+    path('thanks/', views.thanks, name='thanks'),
 ]
