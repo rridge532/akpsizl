1816d6675b91ba18e75fbf9a21154ebc1fee55cc
Robert <robertridge3@gmail.com>
2019-08-22 22:34:30 -0500

    init broth portal page

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='base/index.html', extra_context={'landing': 'landing', 'headerclass': 'alt'}), name='index'),
+    path('portal', TemplateView.as_view(template_name='base/brotherportal.html'), name='portal'),
     path('generic.html', TemplateView.as_view(template_name='base/generic.html'), name='generic'),
     path('elements.html', TemplateView.as_view(template_name='base/elements.html'), name='elements'),
     path('contact.html', TemplateView.as_view(template_name='base/contact.html'), name='contact'),
