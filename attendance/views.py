caad17ff0826bc71469a675d5e1cb023a542ce9e
robert532 <33236215+robert532@users.noreply.github.com>
2020-02-01 18:08:43 -0600

    refactor(urls): use reverse instead of explicit paths

--- attendance/views.py
+++ attendance/views.py
@@ -22,7 +22,8 @@ def qrcodeimage(request, eventid, inorout):
     if not inorout == 'sign-in' and not inorout == 'sign-out':
         raise Exception('QR code is malformed. Must be sign-in or sign-out')
     event = get_object_or_404(Event, id=eventid)
-    address = 'https://akpsizl.com/attendance/{}/{}/{}/api'.format(event.id, event.slug, inorout)
+    address = reverse('attendance:signinapi', args=(event.id, event.slug, inorout))
+    # oldaddress = 'https://akpsizl.com/attendance/{}/{}/{}/api'.format(event.id, event.slug, inorout)
     qr = qrcode.QRCode()
     qr.add_data(address)
     qr.make(fit=True)
