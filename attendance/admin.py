dbf1061af6a76e7b1abb3ca34b22692148707007
robert532 <33236215+robert532@users.noreply.github.com>
2020-02-01 18:53:01 -0600

    feat(attendance-admin): complete redesign of admin pages

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,44 +1,70 @@
 from django.contrib import admin
-
+from base import filters
 from .models import EventGroup, Event, Signin, TeamMembership
 
 # Register your models here.
 
 class EventInline(admin.TabularInline):
-    model = Event
-    extra = 0
     exclude = ['slug']
+    extra = 0
+    model = Event
     ordering = ['-date']
 
-# class SigninInline(admin.TabularInline):
-#     model = Signin
-#     readonly_fields = ['user', 'signintime', 'signouttime']
-#     extra = 0
-#     exclude = ['comment']
-#     classes = ['collapse']
+class SigninInline(admin.TabularInline):
+    autocomplete_fields = ['user']
+    classes = ['collapse']
+    exclude = ['comment']
+    extra = 1
+    model = Signin
+    verbose_name = 'sign-in'
+    verbose_name_plural = 'New sign-in'
+
+    def has_change_permission(self, request, obj=None):
+        return False
+    
+    def has_view_permission(self, request, obj=None):
+        return False
 
 class EventGroupAdmin(admin.ModelAdmin):
+    actions = None
     fieldsets = [
-        (None,               {'fields': ['name']}),
-        ('Required Credits', {'fields': ['needed_credits', 'senior_credits'], 'classes': ['collapse']}),
+        (None, {
+            'fields': ['name']
+        }),
+        ('Required Credits', {
+            'fields': ['needed_credits', 'senior_credits'],
+            'classes': ['collapse']
+        }),
     ]
     inlines = [EventInline]
     list_display = [field.name for field in EventGroup._meta.fields if field.name != 'id']
+    list_editable = [field.name for field in EventGroup._meta.fields if field.name not in ['id','name']]
     ordering = ['name']
-    actions = None
+    search_fields = ['name']
 
 class EventAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['group']
+    date_hierarchy = 'date'
+    inlines = [SigninInline]
     list_display = [field.name for field in Event._meta.fields if field.name not in ['id', 'slug']]
-    ordering = ['-date', 'group', 'name']
     list_filter = ['group']
+    ordering = ['-date', 'group', 'name']
     readonly_fields = ['slug']
     search_fields = ['name']
-    # inlines = [SigninInline]
 
 class SigninAdmin(admin.ModelAdmin):
-    list_display = ['event', 'user', 'signintime', 'signouttime', 'attendance_duration']
+    autocomplete_fields = ['event','user']
+    date_hierarchy = 'signintime'
+    list_display = ['user','event','signintime','signouttime','attendance_duration']
+    list_filter = [
+        ('event',filters.RelatedDropdownFilter),
+        ('user',filters.RelatedDropdownFilter),
+        ('event__group',filters.RelatedDropdownFilter),
+    ]
+    list_per_page = 50
     ordering = ['-signintime', 'event', 'user']
-    list_filter = ['event','user']
+    readonly_fields = ['attendance_duration']
+    search_fields = ['event__name','user__username','user__first_name','user__last_name']
 
 admin.site.register(EventGroup, EventGroupAdmin)
 admin.site.register(Event, EventAdmin)
