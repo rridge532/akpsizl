2e172f373f1c9711b665084348c6c52335bd1778
robert532 <33236215+robert532@users.noreply.github.com>
2020-02-01 18:52:34 -0600

    feat(user): new string methods

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,AbstractUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -18,8 +18,9 @@ class Profile(models.Model):
     isloa = models.BooleanField(default=False)
     issenior = models.BooleanField(default=False)
 
+    # TODO: Confirm changing this string doesn't cause unexpected behavior
     def __str__(self):
-        return self.user.username
+        return "%s (%s)" % (self.user.get_full_name(), self.user.username)
 
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
@@ -33,3 +34,20 @@ class SignupToken(models.Model):
 
     def __str__(self):
         return self.token
+
+# TODO: Validate this doesn't break existing templates
+def get_user_string(self):
+    return "%s (%s)" % (self.get_full_name(), self.username)
+
+User.add_to_class("__str__", get_user_string)
+
+
+# TODO: Test converting to the new user model. Can this be done gracefully? Can it be scripted?
+# def user_avatar_path(instance, filename):
+#     return 'user_{0}/avatar/{1}'.format(instance.id, filename)
+
+# class User(AbstractUser):
+#     avatar = models.ImageField(upload_to=user_avatar_path)
+
+#     def __str__(self):
+#         return "%s (%s)" % (self.get_full_name(), self.username)
